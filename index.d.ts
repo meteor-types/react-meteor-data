a251ca48d1996bd7df95e86556d7c8187b862e97
GameMaster <wanahmad.arifin@iproperty.com>
2018-01-15 17:39:34 +0800

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+
+declare module 'meteor/react-meteor-data' {
+  import * as React from 'react';
+
+  type ComponentConstructor<P> = React.ComponentClass<P> | React.StatelessComponent<P>
+
+  function createContainer<InP, D, OutP extends (InP & D)>(
+    options: (props: InP) => D | {getMeteorData: (props: InP) => D, pure?: boolean},
+    reactComponent: ComponentConstructor<OutP>)
+    : ComponentConstructor<InP>;
+
+  function withTracker<InP, D, OutP extends (InP & D)>(
+
+  ): ComponentConstructor<InP>;
+
+  export {createContainer, withTracker};
+}
