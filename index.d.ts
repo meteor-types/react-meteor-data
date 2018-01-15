e0d6294ef9b31b01e70fd72ddaa873471b7a9006
GameMaster <wanahmad.arifin@iproperty.com>
2018-01-15 18:08:18 +0800

    added missing withTracker logic

--- index.d.ts
+++ index.d.ts
@@ -10,8 +10,9 @@ declare module 'meteor/react-meteor-data' {
     : ComponentConstructor<InP>;
 
   function withTracker<InP, D, OutP extends (InP & D)>(
-
+    options: (props: InP) => D | {getMeteorData: (props: InP) => D, pure?: boolean},
+    reactComponent: ComponentConstructor<OutP>
   ): ComponentConstructor<InP>;
 
-  export {createContainer, withTracker};
+  export {withTracker, createContainer};
 }
