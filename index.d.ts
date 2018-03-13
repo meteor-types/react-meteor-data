6e9136bd54c49656d8783e7fef186ac8bbfcfa54
Grandmaster Dev <one.ahmad.arifin@gmail.com>
2018-03-14 00:37:03 +0800

    Updated index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,14 @@
-
 declare module 'meteor/react-meteor-data' {
   import * as React from 'react';
 
-  type ComponentConstructor<P> = React.ComponentClass<P> | React.StatelessComponent<P>
+  //type ComponentConstructor<P> = React.ComponentClass<P> | React.StatelessComponent<P>
+  //
+  // export function createContainer<InP, D, OutP extends (InP & D)>(
+  //   options: (props: InP) => D | {getMeteorData: (props: InP) => D, pure?: boolean},
+  //   reactComponent: ComponentConstructor<OutP>)
+  //   : ComponentConstructor<InP>;
 
-  function createContainer<InP, D, OutP extends (InP & D)>(
-    options: (props: InP) => D | {getMeteorData: (props: InP) => D, pure?: boolean},
-    reactComponent: ComponentConstructor<OutP>)
-    : ComponentConstructor<InP>;
+  type ComponentConstructor<P> = any;
 
-  function withTracker<InP, D, OutP extends (InP & D)>(
-    options: (props: InP) => D | {getMeteorData: (props: InP) => D, pure?: boolean},
-    reactComponent: ComponentConstructor<OutP>
-  ): ComponentConstructor<InP>;
-
-  export {withTracker, createContainer};
+  export function withTracker<P>(args:any):ComponentConstructor<P>;
 }
